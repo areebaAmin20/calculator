6634439db05e81ffde4ecf896341d2d3ae5c1697
areebaAmin20 <156665628+areebaAmin20@users.noreply.github.com>
2024-01-17 16:21:38 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,24 @@
+print("enter +,-,*,//")
+x=input('operations: ')
+a=int(input('number 1: '))
+b=int(input('number 2: '))
+if x == "+":
+    print(a+b)
+elif x == '-':
+    print(a-b)
+elif x == '*':
+    print(a*b)
+elif x == '//':
+    print(a//b)
+
+
+
+
+
+
+
+
+
+
+
+          
